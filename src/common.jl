b1f2adabc8a3672c8560577e0592e93e9a511402
Jose Daniel Lara <jdlara@berkeley.edu>
2023-02-22 10:56:47 -0700

    run formatter

--- src/common.jl
+++ src/common.jl
@@ -2,14 +2,13 @@
 # Build ABA Matrix
 # Build AB Trial
 
-
 function get_ac_branches(sys)
     # Filter out DC Branches here
     return sort!(
-            collect(PSY.get_components(PSY.ACBranch, sys));
-            by = x ->
-                (PSY.get_number(PSY.get_arc(x).from), PSY.get_number(PSY.get_arc(x).to)),
-        )
+        collect(PSY.get_components(PSY.ACBranch, sys));
+        by = x ->
+            (PSY.get_number(PSY.get_arc(x).from), PSY.get_number(PSY.get_arc(x).to)),
+    )
 end
 
 function get_buses(sys)
@@ -18,7 +17,9 @@ end
 
 function validate_linear_solver(linear_solver::String)
     if linear_solver ∉ SUPPORTED_LINEAR_SOLVERS
-        error("Invalid linear solver. Supported linear solvers are: $(SUPPORTED_LINEAR_SOLVERS)")
+        error(
+            "Invalid linear solver. Supported linear solvers are: $(SUPPORTED_LINEAR_SOLVERS)",
+        )
     end
     return
 end
