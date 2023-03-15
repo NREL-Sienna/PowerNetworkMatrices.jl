b7a3795838774d4864b382fd5ff521abc2b1b110
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-15 14:58:49 -0600

    fix tests

--- test/test_ybus.jl
+++ test/test_ybus.jl
@@ -1,4 +1,5 @@
 @testset "Ybus Matrix" begin
+    sys = PSB.build_system(PSB.PSITestSystems, "c_sys5")
     nodes_5 = nodes5()
     branches_5 = branches5(nodes_5)
     nodes_14 = nodes14()
@@ -21,13 +22,15 @@
         @test isapprox(Ybus14[i[1], i[2]], Ybus14_matpower[i[1], i[2]], atol = 1e-2)
     end
 
+    bus5 = get_component(Bus, sys, "nodeE")
+    set_number!(bus5, 10)
     Y5NS = Ybus(sys)
-    @test isapprox(getindex(Y5NS, 10, 4), -3.3336667 + 33.336667im, atol = 1e-4)
+    @test isapprox(Y5NS[10, 4], -3.3336667 + 33.336667im, atol = 1e-4)
 
-    #Y5NS = Ybus([branches_5[b] for b in Br5NS_ids], [nodes_5[b] for b in Bu5NS_ids]);
-    #for buf in Bu5NS_ids, but in Bu5NS_ids
-    #    @test isapprox(getindex(Y5NS, buf, but), Ybus5_matpower[buf,but], atol=1e-3)
-    #end
+    Y5NS = Ybus([branches_5[b] for b in Br5NS_ids], [nodes_5[b] for b in Bu5NS_ids])
+    for buf in Bu5NS_ids, but in Bu5NS_ids
+        @test isapprox(Y5NS[buf, but], Ybus5_matpower[buf, but], atol = 1e-3)
+    end
 
     @test Ybus5[nodes_5[1], nodes_5[2]] == (-3.5234840209999647 + 35.234840209999646im)
 
