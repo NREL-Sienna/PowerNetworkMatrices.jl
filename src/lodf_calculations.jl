dffcf43a7a132594a013faaff9131489b8d9b176
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-15 13:23:30 -0600

    restore multiplication

--- src/lodf_calculations.jl
+++ src/lodf_calculations.jl
@@ -8,12 +8,15 @@ struct LODF{Ax, L <: NTuple{2, Dict}} <: PowerNetworkMatrix{Float64}
     lookup::L
 end
 
-function _buildlodf(branches, nodes, bus_lookup::Dict{Int, Int}, dist_slack::Array{Float64} = [0.1])
+function _buildlodf(
+    branches,
+    nodes,
+    bus_lookup::Dict{Int, Int},
+    dist_slack::Array{Float64} = [0.1],
+)
     linecount = length(branches)
     ptdf, a = calculate_PTDF_matrix_DENSE(branches, nodes, bus_lookup, dist_slack)
-    @error size(a)
-    ptdf_denominator = ptdf*a
-    #ptdf_denominator = gemm('N', 'N', ptdf, Matrix{Float64}(a))
+    ptdf_denominator = ptdf * transpose(a)
     for iline in 1:linecount
         if (1.0 - ptdf_denominator[iline, iline]) < 1.0E-06
             ptdf_denominator[iline, iline] = 0.0
@@ -23,7 +26,7 @@ function _buildlodf(branches, nodes, bus_lookup::Dict{Int, Int}, dist_slack::Arr
         Matrix{Float64}(LinearAlgebra.I, linecount, linecount) -
         Array(LinearAlgebra.Diagonal(ptdf_denominator)),
     )
-    lodf = gemm('N', 'N', H, getri!(Dem, dipiv))
+    lodf = gemm('N', 'N', ptdf_denominator, getri!(Dem, dipiv))
     lodf =
         lodf - Array(LinearAlgebra.Diagonal(lodf)) -
         Matrix{Float64}(LinearAlgebra.I, linecount, linecount)
