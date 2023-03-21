b17d19f34d1a8e184bca0cf3225f450e1e9b4332
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-21 14:00:25 -0600

    add cache warning

--- src/definitions.jl
+++ src/definitions.jl
@@ -3,4 +3,6 @@ const GODERYA_MAX_PERFORMANCE_NODE = 15_000
 
 const KiB = 1024
 const MiB = KiB * KiB
-const MAX_CACHE_SIZE = 100 * MiB
+const GiB = MiB * KiB
+const MAX_CACHE_SIZE_MiB = 100
+const ROW_PERSISTENT_CACHE_WARN = 1 * GiB
