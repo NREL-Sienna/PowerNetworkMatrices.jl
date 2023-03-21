c0f917e5f0c2b5c23ce914943917e8aef23b2f1f
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-21 12:15:59 -0600

    add cache methods for VirtualPTDF

--- src/definitions.jl
+++ src/definitions.jl
@@ -1,2 +1,6 @@
 const SUPPORTED_LINEAR_SOLVERS = ["KLU", "MKLPardiso", "Dense"]
 const GODERYA_MAX_PERFORMANCE_NODE = 15_000
+
+const KiB = 1024
+const MiB = KiB * KiB
+const MAX_CACHE_SIZE = 100 * MiB
