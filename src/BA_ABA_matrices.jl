4ab31618b89c726f2c20607996a7845a593ed99b
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-21 10:46:46 -0600

    fix a few bugs

--- src/BA_ABA_matrices.jl
+++ src/BA_ABA_matrices.jl
@@ -1,6 +1,7 @@
 # BA matrix ##################################################################
 struct BA_Matrix <: PowerNetworkMatrix{Float64}
     data::SparseArrays.SparseMatrixCSC{Float64, Int}
+    ref_bus_positions::Vector{Int}
 end
 
 # create the BA matrix
@@ -11,7 +12,7 @@ function BA_Matrix(sys::PSY.System)
     bus_lookup = make_ax_ref(buses)
 
     data = calculate_BA_matrix(branches, ref_bus_positions, bus_lookup)
-    return BA_Matrix(data)
+    return BA_Matrix(data, ref_bus_positions)
 end
 
 # ABA matrix #################################################################
@@ -21,9 +22,8 @@ end
 
 # create the ABA matrix
 function ABA_Matrix(A::SparseArrays.SparseMatrixCSC{Int8, Int},
-    BA::SparseArrays.SparseMatrixCSC{T, Int}
-    where {T <: Union{Float32, Float64}},
-    slack_positions::Vector{Int})
+    BA::SparseArrays.SparseMatrixCSC{T, Int},
+    slack_positions::Vector{Int}) where {T <: Union{Float32, Float64}}
     data = calculate_ABA_matrix(A, BA, slack_positions)
     return ABA_Matrix(data)
 end
