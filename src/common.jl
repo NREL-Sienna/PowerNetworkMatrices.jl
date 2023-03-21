3fde4aeb3c2137e88e631a82ecea7bf34c4f4b75
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-21 17:09:48 -0600

    add connectivity methods to common

--- src/common.jl
+++ src/common.jl
@@ -206,3 +206,40 @@ function make_entries_zero!(vector::Vector{Float64}, tol::Float64)
     end
     return vector
 end
+
+function find_subnetworks(M::SparseArrays.SparseMatrixCSC, bus_numbers::Vector{Int})
+    rows = SparseArrays.rowvals(M)
+    _, n = size(M)
+    touched = Set{Int}()
+    bus_groups = Dict{Int, Set{Int}}()
+
+    for j in 1:n
+        row_ix = rows[j]
+        if bus_numbers[row_ix] ∉ touched
+            push!(touched, bus_numbers[row_ix])
+            bus_groups[bus_numbers[row_ix]] = Set{Int}()
+            dfs(row_ix, M, bus_numbers, bus_groups[bus_numbers[row_ix]], touched)
+        end
+    end
+
+    return bus_groups
+end
+
+function dfs(
+    index::Int,
+    M::SparseArrays.SparseMatrixCSC,
+    bus_numbers::Vector{Int},
+    bus_group::Set{Int},
+    touched::Set{Int},
+)
+    rows = SparseArrays.rowvals(M)
+    for j in SparseArrays.nzrange(M, index)
+        row_ix = rows[j]
+        if bus_numbers[row_ix] ∉ touched
+            push!(touched, bus_numbers[row_ix])
+            push!(bus_group, bus_numbers[row_ix])
+            dfs(row_ix, M, bus_numbers, bus_group, touched)
+        end
+    end
+    return
+end
