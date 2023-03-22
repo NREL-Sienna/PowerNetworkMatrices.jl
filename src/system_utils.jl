f9c35a1510c4fd7946f92d8a9b752495e3aff370
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-21 23:42:11 -0600

    reimplement validate connectivity

--- src/system_utils.jl
+++ src/system_utils.jl
@@ -1,22 +1,11 @@
 """
-Finds the set of bus numbers that belong to each connnected component in the System
-"""
-function find_connected_components(sys::PSY.System)
-    a = AdjacencyMatrix(sys; check_connectivity = false)
-    return find_connected_components(a.data, a.lookup[1])
-end
-
-"""
 Checks the network connectivity of the system.
 
 # Keyword arguments
 - `connectivity_method::Function = goderya_connectivity`: Specifies the method used as Goderya's algorithm (`goderya_connectivity`) or depth first search/network traversal (`dfs_connectivity`)
 * Note that the default Goderya method is more efficient, but is resource intensive and may not scale well on large networks.
 """
-function validate_connectivity(
-    sys::PSY.System;
-    connectivity_method::Function = goderya_connectivity,
-)
+function validate_connectivity(sys::PSY.System)
     nodes = sort!(
         collect(
             PSY.get_components(x -> PSY.get_bustype(x) != BusTypes.ISOLATED, PSY.Bus, sys),
@@ -24,10 +13,7 @@ function validate_connectivity(
         by = x -> PSY.get_number(x),
     )
     branches = get_ac_branches(sys)
-    a = AdjacencyMatrix(branches, nodes; check_connectivity = false)
-
-    return validate_connectivity(
-        a;
-        connectivity_method = connectivity_method,
-    )
+    M, bus_lookup = calculate_adjacency(branches, nodes)
+    sbn = find_subnetworks(M, collect(keys(bus_lookup)), Int[])
+    return sbn == 1
 end
