a7e97fa078afb9617890f083221d56c814cdbbc7
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-22 22:14:38 -0600

    add new methods for system convenience

--- src/system_utils.jl
+++ src/system_utils.jl
@@ -6,20 +6,11 @@ Checks the network connectivity of the system.
 * Note that the default Goderya method is more efficient, but is resource intensive and may not scale well on large networks.
 """
 function validate_connectivity(sys::PSY.System)
-    nodes = sort!(
-        collect(
-            PSY.get_components(x -> PSY.get_bustype(x) != BusTypes.ISOLATED, PSY.Bus, sys),
-        );
-        by = x -> PSY.get_number(x),
-    )
-    branches = get_ac_branches(sys)
-    @info "Validating connectivity with depth first search (network traversal)"
-    M, bus_lookup = calculate_adjacency(branches, nodes)
-    sbn = find_subnetworks(M, collect(keys(bus_lookup)))
+    sbn, _ = _find_subnetworks(sys)
     return length(sbn) == 1
 end
 
-function get_subnetworks(sys::PSY.System)
+function _find_subnetworks(sys::PSY.System)
     nodes = sort!(
         collect(
             PSY.get_components(x -> PSY.get_bustype(x) != BusTypes.ISOLATED, PSY.Bus, sys),
@@ -29,5 +20,11 @@ function get_subnetworks(sys::PSY.System)
     branches = get_ac_branches(sys)
     @info "Validating connectivity with depth first search (network traversal)"
     M, bus_lookup = calculate_adjacency(branches, nodes)
-    return find_subnetworks(M, collect(keys(bus_lookup)))
+    return find_subnetworks(M, collect(keys(bus_lookup))), find_slack_positions(nodes)
+end
+
+
+function find_subnetworks(sys::PSY.System)
+    sbn, ref_bus_positions = _find_subnetworks(sys)
+    return assing_reference_buses(sbn, ref_bus_positions)
 end
