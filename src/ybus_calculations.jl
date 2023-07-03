9c468101cce5f3464aa7a45f8bbdec8da999d1e5
Jose Daniel Lara <jdlara@berkeley.edu>
2023-07-03 11:58:12 -0700

    update bustypes to ACBustypes

--- src/ybus_calculations.jl
+++ src/ybus_calculations.jl
@@ -205,3 +205,193 @@ function Ybus(sys::PSY.System; kwargs...)
         kwargs...,
     )
 end
+
+"""
+Nodal incidence matrix (Adjacency) is an N x N matrix describing a power system with N buses. It represents the directed connectivity of the buses in a power system.
+
+The Adjacency Struct is indexed using the Bus Numbers, no need for them to be sequential
+"""
+struct Adjacency{Ax, L <: NTuple{2, Dict}} <: PowerNetworkMatrix{Int}
+    data::SparseArrays.SparseMatrixCSC{Int, Int}
+    axes::Ax
+    lookup::L
+end
+
+"""
+Builds a Adjacency from a collection of buses and branches. The return is an N x N Adjacency Array indexed with the bus numbers.
+
+# Keyword arguments
+- `check_connectivity::Bool`: Checks connectivity of the network using Goderya's algorithm
+"""
+function Adjacency(branches, nodes; check_connectivity::Bool = true, kwargs...)
+    buscount = length(nodes)
+    bus_ax = PSY.get_number.(nodes)
+    axes = (bus_ax, bus_ax)
+    bus_lookup = _make_ax_ref(bus_ax)
+    look_up = (bus_lookup, bus_lookup)
+
+    a = SparseArrays.spzeros(Int, buscount, buscount)
+
+    for b in branches
+        (fr_b, to_b) = get_bus_indices(b, bus_lookup)
+        a[fr_b, to_b] = 1
+        a[to_b, fr_b] = -1
+        a[fr_b, fr_b] = 1
+        a[to_b, to_b] = 1
+    end
+
+    if check_connectivity
+        connected = validate_connectivity(a, nodes, bus_lookup; kwargs...)
+        !connected && throw(DataFormatError("Network not connected"))
+    end
+
+    return Adjacency(a, axes, look_up)
+end
+
+"""
+Builds a Adjacency from the system. The return is an N x N Adjacency Array indexed with the bus numbers.
+
+# Keyword arguments
+- `check_connectivity::Bool`: Checks connectivity of the network using Goderya's algorithm
+- `connectivity_method::Function = goderya_connectivity`: method (`goderya_connectivity` or `dfs_connectivity`) for connectivity validation
+"""
+function Adjacency(sys::PSY.System; check_connectivity::Bool = true, kwargs...)
+    nodes = sort!(
+        collect(
+            PSY.get_components(x -> PSY.get_bustype(x) != ACBusTypes.ISOLATED, PSY.Bus, sys),
+        );
+        by = x -> PSY.get_number(x),
+    )
+    branches = PSY.get_components(PSY.get_available, PSY.Branch, sys)
+    return Adjacency(branches, nodes; check_connectivity = check_connectivity, kwargs...)
+end
+
+function _goderya(ybus::SparseArrays.SparseMatrixCSC)
+    node_count = size(ybus)[1]
+    max_I = node_count^2
+    I, J, val = SparseArrays.findnz(ybus)
+    T = SparseArrays.sparse(I, J, ones(Int, length(val)))
+    T_ = T * T
+    for n in 1:(node_count - 1)
+        I, _, _ = SparseArrays.findnz(T_)
+        if length(I) == max_I
+            @info "The System has no islands"
+            break
+        elseif length(I) < max_I
+            temp = T_ * T
+            I_temp, _, _ = SparseArrays.findnz(temp)
+            if all(I_temp == I)
+                @warn "The system contains islands" maxlog = 1
+            end
+            T_ = temp
+        else
+            @assert false
+        end
+        #@assert n < node_count - 1
+    end
+    return I
+end
+
+"""
+Checks the network connectivity of the system.
+
+# Keyword arguments
+- `connectivity_method::Function = goderya_connectivity`: Specifies the method used as Goderya's algorithm (`goderya_connectivity`) or depth first search/network traversal (`dfs_connectivity`)
+* Note that the default Goderya method is more efficient, but is resource intensive and may not scale well on large networks.
+"""
+function validate_connectivity(
+    sys::PSY.System;
+    connectivity_method::Function = goderya_connectivity,
+)
+    nodes = sort!(
+        collect(
+            PSY.get_components(x -> PSY.get_bustype(x) != ACBusTypes.ISOLATED, PSY.Bus, sys),
+        );
+        by = x -> PSY.get_number(x),
+    )
+    branches = PSY.get_components(PSY.get_available, PSY.Branch, sys)
+    a = Adjacency(branches, nodes; check_connectivity = false)
+
+    return validate_connectivity(
+        a.data,
+        nodes,
+        a.lookup[1];
+        connectivity_method = connectivity_method,
+    )
+end
+
+function validate_connectivity(
+    M,
+    nodes::Vector{PSY.Bus},
+    bus_lookup::Dict{Int64, Int64};
+    connectivity_method::Function = goderya_connectivity,
+)
+    connected = connectivity_method(M, nodes, bus_lookup)
+    return connected
+end
+
+function goderya_connectivity(M, nodes::Vector{PSY.Bus}, bus_lookup::Dict{Int64, Int64})
+    @info "Validating connectivity with Goderya algorithm"
+    length(nodes) > 15_000 &&
+        @warn "The Goderya algorithm is memory intensive on large networks and may not scale well, try `connectivity_method = dfs_connectivity"
+
+    I = _goderya(M)
+
+    node_count = length(nodes)
+    connections = Dict([i => count(x -> x == i, I) for i in Set(I)])
+
+    if length(Set(I)) == node_count
+        connected = true
+        if any(values(connections) .!= node_count)
+            cc = Set(values(connections))
+            @warn "Network has at least $(length(cc)) connected components with $cc nodes"
+            connected = false
+        end
+    else
+        disconnected_nodes = PSY.get_name.(nodes[setdiff(values(bus_lookup), I)])
+        @warn "Principal connected component does not contain:" disconnected_nodes
+        connected = false
+    end
+    return connected
+end
+
+"""
+Finds the set of bus numbers that belong to each connnected component in the System
+"""
+# this function extends the PowerModels.jl implementation to accept a System
+function find_connected_components(sys::PSY.System)
+    a = Adjacency(sys; check_connectivity = false)
+    return find_connected_components(a.data, a.lookup[1])
+end
+
+# this function extends the PowerModels.jl implementation to accept an adjacency matrix and bus lookup
+function find_connected_components(M, bus_lookup::Dict{Int64, Int64})
+    pm_buses = Dict([i => Dict("bus_type" => 1, "bus_i" => b) for (i, b) in bus_lookup])
+
+    arcs = findall((LinearAlgebra.UpperTriangular(M) - LinearAlgebra.I) .!= 0)
+    pm_branches = Dict([
+        i => Dict("f_bus" => a[1], "t_bus" => a[2], "br_status" => 1) for
+        (i, a) in enumerate(arcs)
+    ],)
+
+    data = Dict("bus" => pm_buses, "branch" => pm_branches)
+    cc = PSY.calc_connected_components(data)
+    bus_decode = Dict(value => key for (key, value) in bus_lookup)
+    connected_components = Vector{Set{Int64}}()
+    for c in cc
+        push!(connected_components, Set([bus_decode[b] for b in c]))
+    end
+    return Set(connected_components)
+end
+
+function dfs_connectivity(M, ::Vector{PSY.Bus}, bus_lookup::Dict{Int64, Int64})
+    @info "Validating connectivity with depth first search (network traversal)"
+    cc = find_connected_components(M, bus_lookup)
+    if length(cc) != 1
+        @warn "Network has at least $(length(cc)) connected components with $(length.(cc)) nodes"
+        connected = false
+    else
+        connected = true
+    end
+    return connected
+end
