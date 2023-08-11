a5b8d963841d4149eb5976ef2f98fcf84d5a382a
alefcastelli <alef.castelli@gmail.com>
2023-08-10 19:57:47 -0600

    Fixed get_index for ABA matrix (error if slack buses are selected)

--- src/BA_ABA_matrices.jl
+++ src/BA_ABA_matrices.jl
@@ -94,7 +94,7 @@ function ABA_Matrix(sys::PSY.System; factorize = false)
     bus_ax = [PSY.get_number(bus) for bus in buses]
     bus_ax_ = setdiff(bus_ax, ref_bus_positions)
     axes = (bus_ax_, bus_ax_)
-    bus_ax_ref = make_ax_ref(bus_ax)
+    bus_ax_ref = make_ax_ref(bus_ax_)
     lookup = (bus_ax_ref, bus_ax_ref)
     if factorize
         K = klu(ABA)
@@ -147,4 +147,20 @@ function Base.getindex(
     bus_number::Union{Int, Colon},
 )
     return A.data[bus_number, line_number]
+end
+
+# get_index functions: ABA_Matrix stores a square matrix whose number of rows 
+# and column is equal to the number of the system's buses minus the slack ones,
+# NOTE: bus_1, bus_2 are bus numbers not row and column indices!
+function Base.getindex(A::ABA_Matrix, bus_1, bus_2)
+    if bus_1 in A.ref_bus_positions || bus_2 in A.ref_bus_positions
+        err_msg = string(" Rows and columns related to slack buses are not defined for the ABA matrix. \n",
+            "Indices must be referred to any bus number that is not a slack one. \n",
+            "For the current Systems the reference slack buses are: ", collect(A.ref_bus_positions), ". \n")
+        error(err_msg)
+    else
+        i, j = to_index(A, bus_1, bus_2)
+        return A.data[i, j]
+    end
+    return
 end
