c63b74c1e973144be1aa673a4720c6f5bfaf2642
Jose Daniel Lara <jdlara@berkeley.edu>
2024-01-02 14:59:12 -0700

    remove unnecessarty test

--- test/test_A_BA_ABA_with_radial_lines.jl
+++ test/test_A_BA_ABA_with_radial_lines.jl
@@ -1,27 +1,3 @@
-@testset "Test A matrix with radial lines" begin
-    # A matrix evaluated with radial lines must be the same as the ogirinal A
-    # matrix without the rows and columns related to radiale branches and leaf nodes
-    for name in ["c_sys14", "test_RTS_GMLC_sys"]
-        # load the system
-        sys = PSB.build_system(PSB.PSITestSystems, name)
-        # get the incidence matrix
-        A = IncidenceMatrix(sys)
-        # ... and with radial lines
-        A_rad = IncidenceMatrix(sys; reduce_radial_branches = true)
-        # get inidices for the leaf nodes
-        rb = A_rad.radial_branches
-        bus_numbers = []
-        for i in keys(rb.bus_reduction_map)
-            append!(bus_numbers, collect(rb.bus_reduction_map[i]))
-        end
-        bus_idx = setdiff(1:size(A.data, 2), [A.lookup[2][i] for i in bus_numbers])
-        # ... and radial branches
-        br_idx = setdiff(1:size(A.data, 1), [A.lookup[1][i] for i in rb.radial_branches])
-        # now extract A matrix anc compare
-        @test all(isapprox.(A.data[br_idx, bus_idx], A_rad.data))
-    end
-end
-
 @testset "Test BA matrix with radial lines" begin
     for name in ["c_sys14", "test_RTS_GMLC_sys"]
         # load the system
