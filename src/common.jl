5cc4f877cc2032fe907fbeae81a496b747a2d6ba
jd-lara <jdlara@berkeley.edu>
2024-01-18 11:04:52 -0700

    fixes to subnetworks

--- src/common.jl
+++ src/common.jl
@@ -307,25 +307,22 @@ end
 """
 !!! MISSING DOCUMENTATION !!!
 """
-function assign_reference_buses(
+function assign_reference_buses!(
     subnetworks::Dict{Int, Set{Int}},
-    ref_bus_positions::Set{Int},
+    ref_buses::Vector{Int},
 )
-    if isempty(ref_bus_positions) || length(ref_bus_positions) != length(subnetworks)
+    if isempty(ref_buses) || length(ref_buses) != length(subnetworks)
         @warn "The reference bus positions are not consistent with the subnetworks. References buses will be assigned arbitrarily"
         return deepcopy(subnetworks)
     end
     bus_groups = Dict{Int, Set{Int}}()
     for (bus_key, subnetwork_buses) in subnetworks
-        ref_bus = intersect(ref_bus_positions, subnetwork_buses)
+        ref_bus = intersect(ref_buses, subnetwork_buses)
         if length(ref_bus) == 1
             bus_groups[first(ref_bus)] = pop!(subnetworks, bus_key)
-            continue
         elseif length(ref_bus) == 0
-            @warn "No reference bus in the subnetwork associated with bus $bus_key. References buses will be assigned arbitrarily"
-            return subnetworks
+            @warn "No reference bus in the subnetwork associated with bus $bus_key. Reference bus assigned arbitrarily"
         elseif length(ref_bus) > 1
-            # TODO: still to implement
             error(
                 "More than one reference bus in the subnetwork associated with bus $bus_key",
             )
@@ -334,6 +331,15 @@ function assign_reference_buses(
         end
     end
     return bus_groups
+end
+
+function assign_reference_buses!(
+    subnetworks::Dict{Int, Set{Int}},
+    ref_bus_positions::Set{Int},
+    bus_lookup::Dict{Int, Int},
+)
+    ref_buses = [k for (k, v) in bus_lookup if v in ref_positions]
+    assign_reference_buses!(subnetworks, ref_buses)
 end
 
 """
