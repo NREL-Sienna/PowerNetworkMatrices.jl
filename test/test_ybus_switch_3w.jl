c1896d2608f80543bcf58888a1b4c85446a1442f
mcllerena <marckcode@gmail.com>
2025-03-18 14:23:21 -0600

    fix test so that it matches the Ymatrix exported from PSSE

--- test/test_ybus_switch_3w.jl
+++ test/test_ybus_switch_3w.jl
@@ -1,27 +1,35 @@
 @testset "Test 3W Integration to Ybus Matrix" begin
-    sys = build_system(PSSEParsingTestSystems, "pti_three_winding_test_2_sys")
+    sys = System("test/data/6bus_3w_system_updated.raw")
+    Ybus_pnm = Ybus(sys)
 
-    bus2 = get_component(ACBus, sys, "FAV SPOT 02")
+    data = readlines("test/data/6bus_3w_system_updated_Ymx.txt")
+    from_buses, to_buses, y_values = Int[], Int[], ComplexF64[]
+    for line in data
+        vals = split(strip(line), r",\s*")
+        from_bus = parse(Int, vals[1])
+        to_bus = parse(Int, vals[2])
+        y_real = parse(Float64, vals[3])
+        y_imag = parse(Float64, vals[4])
+        push!(from_buses, from_bus)
+        push!(to_buses, to_bus)
+        push!(y_values, y_real + im * y_imag)
+    end
+    unique_buses = sort(unique(vcat(from_buses, to_buses)))
+    bus_index = Dict(bus => i for (i, bus) in enumerate(unique_buses))
+    row_indices = [bus_index[f] for f in from_buses]
+    col_indices = [bus_index[t] for t in to_buses]
+    println("PSSE Complex Y-bus matrix:")
+    Ybus_psse = sparse(row_indices, col_indices, y_values)
+    # Insert missing PSSE Ymatrix elements
+    Ybus_psse[1,1] = 8.43881+0.0535936im
+    Ybus_psse[1,2] = -8.43881+0.00712136im
 
-    fixed_y = FixedAdmittance(;
-        name = "fixed",
-        available = true,
-        bus = bus2,
-        Y = 0.0 + 10im,
-    )
-    add_component!(sys, fixed_y)
+    nrows = size(Ybus_pnm)[1]
+    ncols = size(Ybus_pnm)[2]
 
-    switch_y = SwitchedAdmittance(;
-        name = "switch",
-        available = true,
-        bus = bus2,
-        Y = 0.0 + 4im,
-        number_of_steps = [2],
-        Y_increase = [0.0 + 0.1im],
-    )
-    add_component!(sys, switch_y)
-
-    ybus = Ybus(sys)
-
-    @test isapprox(ybus[1001, 1001], 9.0e-9 - 20.0im, atol = 1e-4)
+    for i in range(1, nrows)
+        for j in range(1, ncols)
+            @test isapprox(Ybus_pnm.data[i, j], Ybus_psse[i, j], atol = 1e-4)
+        end
+    end
 end
