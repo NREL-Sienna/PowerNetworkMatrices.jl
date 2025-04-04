a24ce2facdcf5ad87fbb3b59fb2f0b6b6f12c9b7
m-bossart <bossart.matt@gmail.com>
2025-04-04 15:20:36 -0700

    pass around networkreduction

--- src/definitions.jl
+++ src/definitions.jl
@@ -8,4 +8,4 @@ const ROW_PERSISTENT_CACHE_WARN = 1 * GiB
 
 DEFAULT_LODF_CHUNK_SIZE = 18_000
 
-IS.@scoped_enum(NetworkReductionTypes, ZERO_IMPEDANCE = 1, RADIAL = 2, WARD = 3) # Performing WARD and then RADIAL isn't supported
+IS.@scoped_enum(NetworkReductionTypes, BREAKER_SWITCH = 1, RADIAL = 2, WARD = 3) 
