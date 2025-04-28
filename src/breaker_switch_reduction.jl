13a7726c9e7e11120ff82fc5205ec035852a7778
m-bossart <bossart.matt@gmail.com>
2025-04-28 13:25:22 -0600

    breaker switch reduction

--- src/breaker_switch_reduction.jl
+++ src/breaker_switch_reduction.jl
@@ -15,33 +15,36 @@ function get_breaker_switch_reduction(sys::PSY.System; prior_reduction = Network
     bus_map = A.lookup[2]
     bus_reduction_map_index = Dict{Int, Set{Int}}(k => Set{Int}() for k in keys(bus_map))
     for br in get_ac_branches(sys, prior_reduction.removed_branches)
-        r = PSY.get_r(br)
-        x = PSY.get_x(br)
-        if r == 0 && x == 0
-            from_bus_number = PSY.get_number(PSY.get_from(PSY.get_arc(br)))
-            to_bus_number = PSY.get_number(PSY.get_to(PSY.get_arc(br)))
-            from_bus_reduced = !haskey(bus_reduction_map_index, from_bus_number)
-            to_bus_reduced = !haskey(bus_reduction_map_index, to_bus_number)
-            if from_bus_reduced && to_bus_reduced
-                continue
-            elseif from_bus_reduced && !to_bus_reduced
-                pop!(bus_reduction_map_index, to_bus_number)
-                reduction_set = Set{Int}(to_bus_number)
-                new_number = _find_reduced(bus_reduction_map_index, from_bus_number)
-                union!(bus_reduction_map_index[new_number], reduction_set)
-            elseif !from_bus_reduced && to_bus_reduced
-                pop!(bus_reduction_map_index, from_bus_number)
-                reduction_set = Set{Int}(from_bus_number)
-                new_number = _find_reduced(bus_reduction_map_index, to_bus_number)
-                union!(bus_reduction_map_index[new_number], reduction_set)
+        if isa(br, PSY.DiscreteControlledACBranch)
+            r = PSY.get_r(br)
+            x = PSY.get_x(br)
+            status = PSY.get_branch_status(br)
+            if r == 0.0 && x < ZERO_IMPEDANCE_LINE_REACTANCE_THRESHOLD
+                if status == PSY.DiscreteControlledBranchStatus.CLOSED
+                    from_bus_number = PSY.get_number(PSY.get_from(PSY.get_arc(br)))
+                    to_bus_number = PSY.get_number(PSY.get_to(PSY.get_arc(br)))
+                    from_bus_reduced = !haskey(bus_reduction_map_index, from_bus_number)
+                    to_bus_reduced = !haskey(bus_reduction_map_index, to_bus_number)
+                    if from_bus_reduced && !to_bus_reduced
+                        pop!(bus_reduction_map_index, to_bus_number)
+                        reduction_set = Set{Int}(to_bus_number)
+                        new_number = _find_reduced(bus_reduction_map_index, from_bus_number)
+                        union!(bus_reduction_map_index[new_number], reduction_set)
+                    elseif !from_bus_reduced && to_bus_reduced
+                        pop!(bus_reduction_map_index, from_bus_number)
+                        reduction_set = Set{Int}(from_bus_number)
+                        new_number = _find_reduced(bus_reduction_map_index, to_bus_number)
+                        union!(bus_reduction_map_index[new_number], reduction_set)
+                    elseif !from_bus_reduced && !to_bus_reduced
+                        pop!(bus_reduction_map_index, from_bus_number)
+                        reduction_set = Set{Int}(from_bus_number)
+                        union!(bus_reduction_map_index[to_bus_number], reduction_set)
+                    end
+                end
+                push!(removed_branches, PSY.get_name(br))
             else
-                pop!(bus_reduction_map_index, from_bus_number)
-                reduction_set = Set{Int}(from_bus_number)
-                union!(bus_reduction_map_index[to_bus_number], reduction_set)
+                push!(retained_branches, PSY.get_name(br))
             end
-            push!(removed_branches, PSY.get_name(br))
-        else
-            push!(retained_branches, PSY.get_name(br))
         end
     end
     reverse_bus_search_map =
