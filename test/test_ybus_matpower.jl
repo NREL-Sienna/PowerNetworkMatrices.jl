e9e45a44b74bb52568842874be4cb07ccc625e6d
m-bossart <bossart.matt@gmail.com>
2025-05-29 18:02:01 -0700

    refactor validation tests psse + matpower

---
+++ test/test_ybus_matpower.jl
@@ -0,0 +1,27 @@
+using DelimitedFiles
+
+@testset "Ybus - ACTIVSg10k" begin 
+    sys = System(joinpath(pwd(), "test", "test_data", "case_ACTIVSg10k.m")) 
+    matpower_cols = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_cols.csv"), Int64)
+    matpower_rows = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_rows.csv"), Int64)
+    matpower_vals_re = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_vals_re.csv"))
+    matpower_vals_im = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_vals_im.csv"))
+    ybus_pnm = Ybus(sys)
+    @test nnz(ybus_pnm.data) == length(filter(!iszero, matpower_vals_re .+ im .* matpower_vals_im )) 
+    for (col, row, val_re, val_im) in zip(matpower_cols, matpower_rows, matpower_vals_re, matpower_vals_im)
+        @test isapprox(ybus_pnm.data[row, col], Complex(val_re, val_im))
+    end 
+end 
+
+@testset "Ybus - CATS" begin 
+    sys = System(joinpath(pwd(), "test", "test_data", "CaliforniaTestSystem.m")) 
+    matpower_cols = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_cols.csv"), Int64)
+    matpower_rows = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_rows.csv"), Int64)
+    matpower_vals_re = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_vals_re.csv"))
+    matpower_vals_im = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_vals_im.csv"))
+    ybus_pnm = Ybus(sys)
+    @test nnz(ybus_pnm.data) == length(filter(!iszero, matpower_vals_re .+ im .* matpower_vals_im )) 
+    for (col, row, val_re, val_im) in zip(matpower_cols, matpower_rows, matpower_vals_re, matpower_vals_im)
+        @test isapprox(ybus_pnm.data[row, col], Complex(val_re, val_im))
+    end 
+end 
