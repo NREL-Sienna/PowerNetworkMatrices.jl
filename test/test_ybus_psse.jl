820444e1f2568c006ef397f8898f797c098f7d8e
m-bossart <bossart.matt@gmail.com>
2025-05-29 18:02:01 -0700

    refactor validation tests psse + matpower

--- test/test_ybus_psse.jl
+++ test/test_ybus_psse.jl
@@ -2,11 +2,10 @@ using SparseArrays
 
 function parse_psse_ybus(path)
     data = readlines(path)
-    from_buses, to_buses, y_values = Int[], Int[], ComplexF64[]
+    row_buses, col_buses, y_values = Int[], Int[], ComplexF64[]
     reduced_bus_pairs = []
     values_done = false
     for line in data
-        @warn line
         if line == " ZERO IMPEDANCE LINE CONNECTED BUSES (BUS IN MATRIX LISTED FIRST):"
             values_done = true
         end
@@ -17,66 +16,113 @@ function parse_psse_ybus(path)
                 push!(reduced_bus_pairs, (parse(Int, vals[1]), parse(Int, vals[2])))
             else
                 vals = split(strip(line), r",\s*")
-                from_bus = parse(Int, vals[1])
-                to_bus = parse(Int, vals[2])
+                row_bus = parse(Int, vals[1])
+                col_bus = parse(Int, vals[2])
                 y_real = parse(Float64, vals[3])
                 y_imag = parse(Float64, vals[4])
-                push!(from_buses, from_bus)
-                push!(to_buses, to_bus)
+                push!(row_buses, row_bus)
+                push!(col_buses, col_bus)
                 push!(y_values, y_real + im * y_imag)
             end
         end
     end
-    unique_buses = sort(unique(vcat(from_buses, to_buses)))
+    unique_buses = sort(unique(vcat(row_buses,col_buses)))
     bus_index = Dict(bus => i for (i, bus) in enumerate(unique_buses))
-    #TODO - manual swap because psse assigns star bus based on sorted 3wtransformer name;
-    bus_index[1100002] = 13
-    bus_index[1100001] = 14
-    row_indices = [bus_index[f] for f in from_buses]
-    col_indices = [bus_index[t] for t in to_buses]
-    Ybus_psse = SparseArrays.sparse(row_indices, col_indices, y_values)
-    return Ybus_psse, reduced_bus_pairs
-end
+    row_indices = [bus_index[f] for f in row_buses]
+    col_indices = [bus_index[t] for t in col_buses]
+    return row_indices, col_indices, y_values, reduced_bus_pairs
+end 
 
-sys = System(joinpath(pwd(), "test", "test_data", "modified_14bus_system.raw"))
-Ybus_pnm = Ybus(sys)
-nr = Ybus_pnm.network_reduction
 
-# Test the NetworkReduction:
-#Test breaker/switch reduction: 
-@test length(nr.bus_reduction_map) == 14
-@test length(nr.reverse_bus_search_map) == 2
-#Test direct branch maps (1:1):
-@test length(keys(nr.direct_branch_map)) == length(keys(nr.reverse_direct_branch_map)) == 13
-#Test double circuit maps:
-@test length(nr.parallel_branch_map) == 1
-@test length(nr.parallel_branch_map[(106, 111)]) == 2
-@test length(nr.reverse_parallel_branch_map) == 2
-#Test 3WT maps (1:1):
-@test length(nr.transformer3W_map) == length(nr.reverse_transformer3W_map) == 6
-#Test adjacency and Ybus have same non-zero elements
-findnz(Ybus_pnm.data)[1] == findnz(Ybus_pnm.adjacency_data)[1]
-findnz(Ybus_pnm.data)[2] == findnz(Ybus_pnm.adjacency_data)[2]
+@testset "14 bus system" begin 
+    sys = System(joinpath(pwd(), "test", "test_data", "modified_14bus_system.raw"))  
+    Ybus_pnm = Ybus(sys; check_connectivity=false)
+    ref_bus_numbers = [get_number(x) for x in get_components(x->get_bustype(x) == PSY.ACBusTypes.REF, ACBus, sys)]
+    skip_indices = indexin(ref_bus_numbers, Ybus_pnm.axes[1])
+    n_ref_bus_elements = nnz(Ybus_pnm.data[skip_indices, :])
+    nr = Ybus_pnm.network_reduction;
 
-# Compare with PSSE
-Ybus_psse, reduced_bus_pairs_psse =
-    parse_psse_ybus("test/test_data/modified_14bus_system_Ymx_SWTBRK.txt")
-# Compare breaker/switch reductions
-for x in reduced_bus_pairs_psse
-    @test (haskey(nr.reverse_bus_search_map, x[1])) ||
-          (haskey(nr.reverse_bus_search_map, x[2]))
-end
-#Test Ybus values: 
-nrows = size(Ybus_pnm)[1]
-ncols = size(Ybus_pnm)[2]
-for i in range(1, nrows)
-    if i ∈ [1, 7]           #TODO - Not getting entries for first row from psse... Ref bus rows. 
-        continue
+    # Test the NetworkReduction:
+    #Test breaker/switch reduction: 
+    @test length(nr.bus_reduction_map) == 14
+    @test length(nr.reverse_bus_search_map) == 2
+    #Test direct branch maps (1:1):
+    @test length(keys(nr.direct_branch_map)) == length(keys(nr.reverse_direct_branch_map)) == 13
+    #Test double circuit maps:
+    @test length(nr.parallel_branch_map) == 1
+    @test length(nr.parallel_branch_map[(106, 111)]) == 2
+    @test length(nr.reverse_parallel_branch_map) == 2
+    #Test 3WT maps (1:1):
+    @test length(nr.transformer3W_map) == length(nr.reverse_transformer3W_map) == 6
+    #Test adjacency and Ybus have same non-zero elements
+    findnz(Ybus_pnm.data)[1] == findnz(Ybus_pnm.adjacency_data)[1]
+    findnz(Ybus_pnm.data)[2] == findnz(Ybus_pnm.adjacency_data)[2]
+
+    # Compare with PSSE
+    row_indices, col_indices, y_values, reduced_bus_pairs_psse =
+        parse_psse_ybus("test/test_data/modified_14bus_system_Ymx_SWTBRK.txt")  # NOTE: manually changed the ybus numbers in this file. 
+    # Compare breaker/switch reductions
+    for x in reduced_bus_pairs_psse
+        @test (haskey(nr.reverse_bus_search_map, x[1])) ||
+            (haskey(nr.reverse_bus_search_map, x[2]))
     end
-    for j in range(1, ncols)
-        if i == 11 && j == 11       #problem with SwitchedAdmittance; needs further debug with PSSE
-            continue
-        end
-        @test isapprox(Ybus_pnm.data[i, j], Ybus_psse[i, j], atol = 1e-3)
+    #Test number of nonzero elements matches
+    @test nnz(Ybus_pnm.data) == length(filter(!iszero, y_values)) + n_ref_bus_elements
+    #Test values match 
+    for (row_ix, col_ix, val) in zip(row_indices, col_indices, y_values)
+        @test isapprox(Ybus_pnm.data[row_ix, col_ix], val, atol = 1e-3)
+    end 
+end 
+
+
+@testset "14 bus system - re-exported from PowerFlows.jl" begin 
+    sys = System(joinpath(pwd(), "test", "test_data", "exported_modified_case14_sys 1.raw"))  
+    Ybus_pnm = Ybus(sys)
+    ref_bus_numbers = [get_number(x) for x in get_components(x->get_bustype(x) == PSY.ACBusTypes.REF, ACBus, sys)]
+    skip_indices = indexin(ref_bus_numbers, Ybus_pnm.axes[1])
+    n_ref_bus_elements = nnz(Ybus_pnm.data[skip_indices, :])
+    nr = Ybus_pnm.network_reduction
+
+    #Test adjacency and Ybus have same non-zero elements
+    findnz(Ybus_pnm.data)[1] == findnz(Ybus_pnm.adjacency_data)[1]
+    findnz(Ybus_pnm.data)[2] == findnz(Ybus_pnm.adjacency_data)[2]
+
+    # Compare with PSSE
+    row_indices, col_indices, y_values, reduced_bus_pairs_psse =
+        parse_psse_ybus("test/test_data/exported_modified_case14_sys_Ymx.txt")
+    # Compare breaker/switch reductions
+    for x in reduced_bus_pairs_psse
+        @test (haskey(nr.reverse_bus_search_map, x[1])) ||
+            (haskey(nr.reverse_bus_search_map, x[2]))
     end
-end
+    #Test number of nonzero elements matches
+    @test nnz(Ybus_pnm.data) == length(filter(!iszero, y_values)) + n_ref_bus_elements
+    #Test values match 
+    for (row_ix, col_ix, val) in zip(row_indices, col_indices, y_values)
+        @test isapprox(Ybus_pnm.data[row_ix, col_ix], val, atol = 1e-3)
+    end 
+end 
+
+@testset "WECC 240 bus" begin 
+    sys_240 = System(joinpath(pwd(), "test", "test_data", "240busWECC_2018_PSS33.raw"); runchecks= false)
+    Ybus_pnm = Ybus(sys_240) 
+    ref_bus_numbers = [get_number(x) for x in get_components(x->get_bustype(x) == PSY.ACBusTypes.REF, ACBus, sys_240)]
+    skip_indices = indexin(ref_bus_numbers, Ybus_pnm.axes[1])
+    n_ref_bus_elements = nnz(Ybus_pnm.data[skip_indices, :])
+
+    # Compare with PSSE
+    row_indices, col_indices, y_values, reduced_bus_pairs_psse =
+        parse_psse_ybus("test/test_data/240busWECC_2018_PSS33_Ymatrix.txt")
+
+    # Compare breaker/switch reductions
+    for x in reduced_bus_pairs_psse
+        @test (haskey(nr.reverse_bus_search_map, x[1])) ||
+            (haskey(nr.reverse_bus_search_map, x[2]))
+    end
+    #Test number of nonzero elements matches
+    @test nnz(Ybus_pnm.data) == length(filter(!iszero, y_values)) + n_ref_bus_elements
+    #Test values match 
+    for (row_ix, col_ix, val) in zip(row_indices, col_indices, y_values)
+        @test isapprox(Ybus_pnm.data[row_ix, col_ix], val, atol = 1e-3)
+    end 
+end 
