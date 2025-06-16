e6b4dcb3b0739e32aafca349c6d9e9b365fe7cdc
m-bossart <bossart.matt@gmail.com>
2025-06-16 15:54:58 -0700

    use case builder for 10k system

--- test/test_ybus_matpower.jl
+++ test/test_ybus_matpower.jl
@@ -1,27 +1,40 @@
 using DelimitedFiles
 
-@testset "Ybus - ACTIVSg10k" begin 
-    sys = System(joinpath(pwd(), "test", "test_data", "case_ACTIVSg10k.m")) 
-    matpower_cols = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_cols.csv"), Int64)
-    matpower_rows = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_rows.csv"), Int64)
+@testset "Ybus - ACTIVSg10k" begin
+    sys = build_system(MatpowerTestSystems, "matpower_ACTIVSg10k_sys")
+    matpower_cols =
+        readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_cols.csv"), Int64)
+    matpower_rows =
+        readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_rows.csv"), Int64)
     matpower_vals_re = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_vals_re.csv"))
     matpower_vals_im = readdlm(joinpath(pwd(), "test", "test_data", "ybus_10k_vals_im.csv"))
     ybus_pnm = Ybus(sys)
-    @test nnz(ybus_pnm.data) == length(filter(!iszero, matpower_vals_re .+ im .* matpower_vals_im )) 
-    for (col, row, val_re, val_im) in zip(matpower_cols, matpower_rows, matpower_vals_re, matpower_vals_im)
+    @test nnz(ybus_pnm.data) ==
+          length(filter(!iszero, matpower_vals_re .+ im .* matpower_vals_im))
+    for (col, row, val_re, val_im) in
+        zip(matpower_cols, matpower_rows, matpower_vals_re, matpower_vals_im)
         @test isapprox(ybus_pnm.data[row, col], Complex(val_re, val_im))
-    end 
-end 
+    end
+end
 
-@testset "Ybus - CATS" begin 
-    sys = System(joinpath(pwd(), "test", "test_data", "CaliforniaTestSystem.m")) 
-    matpower_cols = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_cols.csv"), Int64)
-    matpower_rows = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_rows.csv"), Int64)
-    matpower_vals_re = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_vals_re.csv"))
-    matpower_vals_im = readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_vals_im.csv"))
+@testset "Ybus - CATS" begin
+    sys = System(joinpath(pwd(), "test", "test_data", "CaliforniaTestSystem.m"))
+    matpower_cols =
+        readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_cols.csv"), Int64)
+    matpower_rows =
+        readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_rows.csv"), Int64)
+    matpower_vals_re =
+        readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_vals_re.csv"))
+    matpower_vals_im =
+        readdlm(joinpath(pwd(), "test", "test_data", "ybus_cats_vals_im.csv"))
     ybus_pnm = Ybus(sys)
-    @test nnz(ybus_pnm.data) == length(filter(!iszero, matpower_vals_re .+ im .* matpower_vals_im )) 
-    for (col, row, val_re, val_im) in zip(matpower_cols, matpower_rows, matpower_vals_re, matpower_vals_im)
+    @test nnz(ybus_pnm.data) ==
+          length(filter(!iszero, matpower_vals_re .+ im .* matpower_vals_im))
+    for (col, row, val_re, val_im) in
+        zip(matpower_cols, matpower_rows, matpower_vals_re, matpower_vals_im)
         @test isapprox(ybus_pnm.data[row, col], Complex(val_re, val_im))
+    end
+end
+
     end 
 end 
