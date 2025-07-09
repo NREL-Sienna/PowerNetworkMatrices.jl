239c13363d21640262aef2887f1f475ae3782f75
m-bossart <bossart.matt@gmail.com>
2025-07-09 12:19:33 -0700

    change dist_slack interface, pass virtual_ptdf_with_radial tests

--- test/utils/test_utils.jl
+++ test/utils/test_utils.jl
@@ -9,7 +9,8 @@ function brute_force_ptdf(sys::System; tol = 1e-9)
     for bus_i in 1:size(data.bus_type, 1)
         data.bus_activepower_injection[bus_i, 1] += tol
         solve_powerflow!(data)
-        ptdf[:, bus_i] = (data.branch_activepower_flow_from_to[:, 1] .- init_branch_p_from_to) ./ tol
+        ptdf[:, bus_i] =
+            (data.branch_activepower_flow_from_to[:, 1] .- init_branch_p_from_to) ./ tol
         data.bus_activepower_injection[bus_i, 1] -= tol
     end
     return ptdf, data.bus_lookup, data.branch_lookup
