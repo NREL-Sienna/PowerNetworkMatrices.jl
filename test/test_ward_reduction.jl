e296fabf949469e41624cca09a361c9bc950804e
m-bossart <bossart.matt@gmail.com>
2025-07-22 11:56:21 -0700

    a lot of new testing for reductions

--- test/test_ward_reduction.jl
+++ test/test_ward_reduction.jl
@@ -73,6 +73,11 @@ end
     study_buses = filter!(x -> digits(x)[end] == 1, bus_numbers)  #study buses are from area 1 
     _basic_test_ward_reduction(sys, study_buses)
     _test_matrices_ward_reduction(sys, study_buses)
+
+    sys = PSB.build_system(PSSEParsingTestSystems, "psse_14_network_reduction_test_system")
+    study_buses = [101, 114, 110, 111]
+    _basic_test_ward_reduction(sys, study_buses)
+    _test_matrices_ward_reduction(sys, study_buses)
 end
 
 #TODO - develop more robust testing for correctness of PTDF
