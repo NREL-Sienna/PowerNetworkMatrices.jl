3df18428df2c7c171addb97fbd005ec2853aa174
m-bossart <bossart.matt@gmail.com>
2025-07-29 12:59:03 -0700

    add new tests for connectivity edge cases (passing)

--- test/test_connectivity.jl
+++ test/test_connectivity.jl
@@ -59,3 +59,89 @@ end
         @test k in values(test_subnetworks)
     end
 end
+
+@testset "Test matrices for connectivity corner cases" begin
+    sys = PSB.build_system(PSISystems, "HVDC_TWO_RTO_RTS_5min_sys")
+    ybus = Ybus(sys)
+    A = IncidenceMatrix(ybus)
+    ptdf = PTDF(sys)
+    lodf = LODF(sys)
+    vptdf = VirtualPTDF(sys)
+    vlodf = VirtualLODF(sys)
+
+    @test length(ybus.subnetwork_axes) == 2
+    @test keys(ybus.subnetwork_axes) == keys(ptdf.subnetwork_axes) ==
+          keys(lodf.subnetwork_axes) == keys(vptdf.subnetwork_axes) ==
+          keys(vlodf.subnetwork_axes)
+    for k in keys(ptdf.subnetwork_axes)
+        @test iszero([ptdf[x, k] for x in PNM.get_arc_axis(ptdf)])
+    end
+    ref_bus_numbers = [
+        get_number(x) for
+        x in PSY.get_components(x -> get_bustype(x) == ACBusTypes.REF, ACBus, sys)
+    ]
+    for ref_bus in ref_bus_numbers
+        @test ref_bus ∈ keys(ybus.subnetwork_axes)
+    end
+end
+
+@testset "Small island corner cases" begin
+    sys = PSB.build_system(PSB.PSITestSystems, "c_sys14")
+    ybus_1 = Ybus(sys)
+    ptdf_1 = PTDF(sys)
+    lodf_1 = LODF(sys)
+    vptdf_1 = VirtualPTDF(sys)
+    vlodf_1 = VirtualLODF(sys)
+
+    sys = build_hvdc_with_single_bus_island()
+    ybus_2 = Ybus(sys)
+    ptdf_2 = PTDF(sys)
+    lodf_2 = LODF(sys)
+    vptdf_2 = VirtualPTDF(sys)
+    vlodf_2 = VirtualLODF(sys)
+
+    sys = build_hvdc_with_small_island()
+    ybus_3 = Ybus(
+        sys;
+        network_reductions = NetworkReduction[RadialReduction(;
+            irreducible_buses = collect(1:14),
+        )],
+    )
+    ptdf_3 = PTDF(
+        sys;
+        network_reductions = NetworkReduction[RadialReduction(;
+            irreducible_buses = collect(1:14),
+        )],
+    )
+    lodf_3 = LODF(
+        sys;
+        network_reductions = NetworkReduction[RadialReduction(;
+            irreducible_buses = collect(1:14),
+        )],
+    )
+    vptdf_3 = VirtualPTDF(
+        sys;
+        network_reductions = NetworkReduction[RadialReduction(;
+            irreducible_buses = collect(1:14),
+        )],
+    )
+    vlodf_3 = VirtualLODF(
+        sys;
+        network_reductions = NetworkReduction[RadialReduction(;
+            irreducible_buses = collect(1:14),
+        )],
+    )
+
+    for i in ptdf_1.axes[1], j in ptdf_1.axes[2]
+        @test ptdf_1[j,i] == ptdf_2[j,i] == ptdf_3[j,i]
+    end 
+    for i in lodf_1.axes[1], j in lodf_1.axes[2]
+        @test lodf_1[i, j] == lodf_2[i,j] == lodf_3[i,j]
+    end 
+    for i in vptdf_1.axes[1], j in vptdf_1.axes[2]
+        @test vptdf_1[i,j] == vptdf_2[i,j] == vptdf_3[i,j]
+    end 
+    for i in vlodf_1.axes[1], j in vlodf_1.axes[2]
+        @test vlodf_1[i, j] == vlodf_2[i,j] == vlodf_3[i,j]
+    end 
+end
