44054c5f8441f08b8a541299a9aa61cee4e404d5
m-bossart <bossart.matt@gmail.com>
2025-07-29 17:52:40 -0700

    add back dfs, add option for subnetwork algorithm

--- test/test_connectivity.jl
+++ test/test_connectivity.jl
@@ -145,3 +145,14 @@ end
         @test vlodf_1[i, j] == vlodf_2[i, j] == vlodf_3[i, j]
     end
 end
+
+@testset "Subnetwork algorithms" begin 
+    sys = build_hvdc_with_small_island()
+    ybus = @test_logs (:info, r"Finding subnetworks via iterative union find") match_mode = :any  Ybus(sys)
+    ybus = @test_logs (:info, r"Finding subnetworks via depth first search") match_mode = :any  Ybus(sys; subnetwork_algorithm = depth_first_search)
+
+    sub_1 = PNM.find_subnetworks(ybus.data, ybus.axes[1]; subnetwork_algorithm = depth_first_search)
+    sub_2 = PNM.find_subnetworks(ybus.data, ybus.axes[1]; subnetwork_algorithm = iterative_union_find)
+    @test sub_1 == sub_2
+end 
+
