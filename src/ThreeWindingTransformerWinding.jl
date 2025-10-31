0e9c5bad8c8929c8ac8951201b55f1a8876b76a7
Jose Daniel Lara <jdlara@berkeley.edu>
2025-10-30 22:24:27 -0600

    move methods for 3WT

--- src/ThreeWindingTransformerWinding.jl
+++ src/ThreeWindingTransformerWinding.jl
@@ -13,10 +13,23 @@ winding components for network matrix construction and analysis purposes.
 # Note
 This is an internal object and should not be constructed directly by users or added to a system.
 """
-struct ThreeWindingTransformerWinding{T<:PSY.ThreeWindingTransformer} <: PSY.ACTransmission
+struct ThreeWindingTransformerWinding{T <: PSY.ThreeWindingTransformer} <:
+       PSY.ACTransmission
     transformer::T
     winding_number::Int
 end
 
 get_transformer(tw::ThreeWindingTransformerWinding) = tw.transformer
 get_winding_number(tw::ThreeWindingTransformerWinding) = tw.winding_number
+
+function get_name(three_wt_winding::ThreeWindingTransformerWinding)
+    transformer = get_transformer(three_wt_winding)
+    winding = get_winding_number(three_wt_winding)
+    return PSY.get_name(transformer) * "_winding_$winding"
+end
+
+function get_series_susceptance(segment::ThreeWindingTransformerWinding)
+    tfw = get_transformer(segment)
+    winding_int = get_winding_number(segment)
+    return PSY.get_series_susceptances(tfw)[winding_int]
+end
