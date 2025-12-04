18d05859c41eeceb611bb3f476fa42d337af05ca
Luke Kiernan <lkiernan@nrel.gov>
2025-12-04 13:43:42 -0700

    test extension code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,11 @@
+# load system-specific packages before PNM: ensures relevant PNM extensions will be loaded.
+@static if (Sys.ARCH === :x86_64 || Sys.ARCH === :i686) && !Sys.isapple()
+    using MKL
+    using Pardiso
+else
+    using AppleAccelerate
+end
+
 using PowerNetworkMatrices
 
 include("PowerNetworkMatricesTests.jl")
