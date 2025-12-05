5d2e5ee6d2b9696614532f19b209986a5fbf443a
Luke Kiernan <86331877+luke-kiernan@users.noreply.github.com>
2025-12-05 10:47:05 -0700

    CI yml syntax fix (#227)
    * CI yml syntax fix
    
    * remove extension-related dependencies from Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,12 @@
 # load system-specific packages before PNM: ensures relevant PNM extensions will be loaded.
 @static if (Sys.ARCH === :x86_64 || Sys.ARCH === :i686) && !Sys.isapple()
+    import Pkg
+    Pkg.add(["MKL", "Pardiso"])
     using MKL
     using Pardiso
 else
+    import Pkg
+    Pkg.add("AppleAccelerate")
     using AppleAccelerate
 end
 
