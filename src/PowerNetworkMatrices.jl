916e9405f48b197775a98e7408e8e58802c17100
Luke Kiernan <lkiernan@nrel.gov>
2026-01-12 15:29:32 -0700

    switch blas message to debug

--- src/PowerNetworkMatrices.jl
+++ src/PowerNetworkMatrices.jl
@@ -120,21 +120,17 @@ function _pardiso_single_LODF! end
 function _create_apple_accelerate_factorization end
 
 function __init__()
-    # Suggest optimal BLAS backend.
-    if ccall(:jl_generating_output, Cint, ()) == 1
-        return
-    end
     blas_config = lowercase(string(LinearAlgebra.BLAS.get_config()))
     if Sys.iswindows() && !contains(blas_config, "mkl")
-        @info """For faster dense matrix operations, consider using MKL:
-                   pkg> add MKL
-                   using MKL  # before any matrix operations
-                 Sparse factorization still uses KLU (recommended)."""
+        @debug """For faster dense matrix operations, consider using MKL:
+                    pkg> add MKL
+                    using MKL  # before any matrix operations
+                  Sparse factorization still uses KLU (recommended)."""
     elseif Sys.isapple() && !contains(blas_config, "accelerate")
-        @info """For faster dense matrix operations, consider using AppleAccelerate:
-                   pkg> add AppleAccelerate
-                   using AppleAccelerate  # before any matrix operations
-                 Sparse factorization still uses KLU (recommended)."""
+        @debug """For faster dense matrix operations, consider using AppleAccelerate:
+                    pkg> add AppleAccelerate
+                    using AppleAccelerate  # before any matrix operations
+                  Sparse factorization still uses KLU (recommended)."""
     end
 end
 
